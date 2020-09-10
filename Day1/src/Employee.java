3d16e98fbd73415032cb9cb6ee3b429b69f6a31f
ChandraVarshini <varshinig18@gmail.com>
2020-09-10 14:26:12 +0530

    commit

---
+++ Day1/src/Employee.java
@@ -0,0 +1,33 @@
+
+public class Employee {
+	private int employeeNumber;
+	private float salary;
+	private String name;
+	
+	public int method1()
+	{
+		return employeeNumber;
+	}
+	
+	public void method2(int employeeNumber)
+			{
+	this.employeeNumber=employeeNumber;
+			}
+	
+	public float method3()
+	{
+		return salary;
+	}
+	public void method4(float salary)
+	{
+		this.salary=salary;
+	}
+	public String method5()
+	{
+		return name;
+	}
+	public void method6(String name)
+	{
+		this.name=name;
+	}
+}
---
+++ Day1/src/MainApp.java
@@ -0,0 +1,13 @@
+
+public class MainApp {
+public static void main(String args[])
+{
+	Employee employee=new Employee();
+	employee.method2(10);
+	employee.method6("Varshini");
+	employee.method4(10000);
+	System.out.println("Employee number is  "+employee.method1());
+	System.out.println("Employee name   "+employee.method5());
+	System.out.println("Employee salary   "+employee.method3());
+}
+}
